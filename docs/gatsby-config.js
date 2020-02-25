3dc774f5d6e1cb3facb66bb3b5e30994673e0b44
henrikwirth <sirhenry91@gmail.com>
2020-02-25 18:26:06 +0100

    Fix some naming

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -4,28 +4,6 @@ module.exports = {
     description: `GatsbyWPGutenberg documentation.`,
   },
   plugins: [
-    // `gatsby-plugin-react-helmet`,
-    // {
-    //   resolve: `gatsby-source-filesystem`,
-    //   options: {
-    //     name: `images`,
-    //     path: `${__dirname}/content/images`,
-    //   },
-    // },
-    // `gatsby-transformer-sharp`,
-    // `gatsby-plugin-sharp`,
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: `WordPress Gutenberg Gatsby documentation`,
-    //     short_name: `wordpress-gutenberg-gatsby-docs`,
-    //     start_url: `/`,
-    //     background_color: `#663399`,
-    //     theme_color: `#663399`,
-    //     display: `minimal-ui`,
-    //     icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-    //   },
-    // },
     {
       resolve: "gatsby-theme-apollo-docs",
       options: {
@@ -41,7 +19,7 @@ module.exports = {
         baseUrl: "https://wpgg-docs.netlify.com",
         root: __dirname,
         description: "GatsbyWPGutenberg documentation",
-        githubRepo: "wpgg-framework/wordpress-gutenberg-gatsby",
+        githubRepo: "GatsbyWPGutenberg/gatsby-wordpress-gutenberg/docs",
         logoLink: "https://docs.mobileui.dev",
         navConfig: {
           "wpgg.netlify.com": {
@@ -70,8 +48,5 @@ module.exports = {
         },
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
