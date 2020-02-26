8d1f1ccb3d05460e8fe53b9046fb7bff832a30d5
Peter Pristas <pristas.peter@gmail.com>
2020-02-26 03:10:29 +0100

    Add dependency param to useSelect

--- plugins/wp-gatsby-gutenberg/lib/src/preview/index.js
+++ plugins/wp-gatsby-gutenberg/lib/src/preview/index.js
@@ -1,61 +1,74 @@
 /**
  * WordPress dependencies
  */
-import { useEffect, useContext, createContext } from "@wordpress/element"
-import { useRegistry, useDispatch, useSelect } from "@wordpress/data"
-import { addFilter } from "@wordpress/hooks"
+import { useEffect, useContext, createContext } from '@wordpress/element';
+import { useRegistry, useDispatch, useSelect } from '@wordpress/data';
+import { addFilter } from '@wordpress/hooks';
 
 // import { registerPlugin } from "@wordpress/plugins"
 // import { PluginPostPublishPanel } from "@wordpress/edit-post"
 
-import store from "./store"
-import { sendPreview } from "./gatsby"
+import store from './store';
+import { sendPreview } from './gatsby';
 
-const CoreBlockContext = createContext(null)
+const CoreBlockContext = createContext(null);
 
 store.subscribe(() => {
-  sendPreview({
-    // client,
-    state: store.getState(),
-  })
-})
+	sendPreview({
+		// client,
+		state: store.getState(),
+	});
+});
 
-addFilter(`editor.BlockEdit`, `plugin-wp-gatsby-gutenberg-preview/BlockEdit`, Edit => {
-  return props => {
-    const registry = useRegistry()
-    const blocks = registry.select(`core/block-editor`).getBlocks()
-    const coreBlock = useContext(CoreBlockContext)
-    const id = useSelect(select => {
-      return select(`core/editor`).getCurrentPostId()
-    })
-    const slug = useSelect(select => {
-      return select(`core/editor`).getEditedPostAttribute(`slug`)
-    })
+addFilter(
+	`editor.BlockEdit`,
+	`plugin-wp-gatsby-gutenberg-preview/BlockEdit`,
+	(Edit) => {
+		return (props) => {
+			const registry = useRegistry();
+			const blocks = registry.select(`core/block-editor`).getBlocks();
+			const coreBlock = useContext(CoreBlockContext);
 
-    const link = useSelect(select => {
-      return select(`core/editor`).getEditedPostAttribute(`link`)
-    })
+			const id = useSelect(
+				(select) => select(`core/editor`).getCurrentPostId(),
+				[]
+			);
+			const slug = useSelect(
+				(select) =>
+					select(`core/editor`).getEditedPostAttribute(`slug`),
+				[]
+			);
+			const link = useSelect(
+				(select) =>
+					select(`core/editor`).getEditedPostAttribute(`link`),
+				[]
+			);
 
-    const { setBlocks } = useDispatch(`wp-gatsby-gutenberg/preview`)
-    const coreBlockId = (coreBlock && coreBlock.attributes.ref && parseInt(coreBlock.attributes.ref, 10)) || null
+			const { setBlocks } = useDispatch(`wp-gatsby-gutenberg/preview`);
+			const coreBlockId =
+				(coreBlock &&
+					coreBlock.attributes.ref &&
+					parseInt(coreBlock.attributes.ref, 10)) ||
+				null;
 
-    useEffect(() => {
-      if (id) {
-        setBlocks({ id, blocks, coreBlockId, slug, link })
-      }
-    }, [blocks, coreBlockId, id])
+			useEffect(() => {
+				if (id) {
+					setBlocks({ id, blocks, coreBlockId, slug, link });
+				}
+			}, [blocks, coreBlockId, id]);
 
-    if (props.name === `core/block`) {
-      return (
-        <CoreBlockContext.Provider value={props}>
-          <Edit {...props}></Edit>
-        </CoreBlockContext.Provider>
-      )
-    }
+			if (props.name === `core/block`) {
+				return (
+					<CoreBlockContext.Provider value={props}>
+						<Edit {...props}></Edit>
+					</CoreBlockContext.Provider>
+				);
+			}
 
-    return <Edit {...props} />
-  }
-})
+			return <Edit {...props} />;
+		};
+	}
+);
 
 // const GatsbyWordpressGutenbergPreview
 
