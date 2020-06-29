95f14c53d5766891149b3d4d9c8f8d33f3a52035
Peter Pristas <pristas.peter@gmail.com>
2020-06-29 12:31:13 +0200

    Add links to homepage

--- examples/gatsby-wordpress-gutenberg-default-example/frontend/src/pages/index.js
+++ examples/gatsby-wordpress-gutenberg-default-example/frontend/src/pages/index.js
@@ -1,18 +1,19 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 export default function Home({ data }) {
-  //highlight-line
   return (
     <Layout>
       <SEO title="home" />
       <h1>My WordPress Blog</h1>
       <h4>Posts</h4>
       {data.allWpPost.nodes.map(node => (
-        <div>
-          <p>{node.title}</p>
+        <div key={node.slug}>
+          <Link to={node.slug}>
+            <h2>{node.title}</h2>
+          </Link>
           <div dangerouslySetInnerHTML={{ __html: node.excerpt }} />
         </div>
       ))}
@@ -20,7 +21,6 @@ export default function Home({ data }) {
   )
 }
 
-//highlight-start
 export const pageQuery = graphql`
   query {
     allWpPost(sort: { fields: [date] }) {
